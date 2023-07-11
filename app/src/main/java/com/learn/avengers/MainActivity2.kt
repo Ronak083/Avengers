f1805d25faa3f3385b86a7c39a2652283a1801ec
ronak <ronakgupta.official@gmail.com>
2023-07-11 19:37:34 +0530

    Created Login and Avengers Assemble page

--- app/src/main/java/com/learn/avengers/MainActivity2.kt
+++ app/src/main/java/com/learn/avengers/MainActivity2.kt
@@ -3,10 +3,9 @@ package com.learn.avengers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title = "Login Page"
+        setContentView(R.layout.avengers)
     }
 }
