633dc0a7f99a24be6dc4e9482814ae788467a7d7
ronak <ronakgupta.official@gmail.com>
2023-07-12 17:09:16 +0530

    Created Message View

--- app/src/main/java/com/learn/avengers/AvengersActivity.kt
+++ app/src/main/java/com/learn/avengers/AvengersActivity.kt
@@ -1,18 +1,36 @@
 package com.learn.avengers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class AvengersActivity : AppCompatActivity() {
 
     var titleName: String? = "Avengers"
+    lateinit var txtMessage: EditText
+    lateinit var btnSend: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.avengers)
+
+        txtMessage = findViewById(R.id.txtMessage)
+        btnSend = findViewById(R.id.btnSend)
 
         if(intent != null){
             titleName = intent.getStringExtra("Name")
         }
         title = titleName
+
+        btnSend.setOnClickListener{
+            val message = txtMessage.text.toString()
+            val intent = Intent(this@AvengersActivity,MessageActivity::class.java)
+            var newMessage = message
+            if(message == null){
+                intent.putExtra("newMessage", newMessage)
+                startActivity(intent)
+            }
+        }
     }
 }
