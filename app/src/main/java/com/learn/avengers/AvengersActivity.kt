82da563474548dabe6fb8e819e617f8ba76ce463
ronak <ronakgupta.official@gmail.com>
2023-07-13 19:35:55 +0530

    Completed Module-1 with all the functional Button and pages i.e. forget passwowrd, Register Yourself

--- app/src/main/java/com/learn/avengers/AvengersActivity.kt
+++ app/src/main/java/com/learn/avengers/AvengersActivity.kt
@@ -1,6 +1,8 @@
 package com.learn.avengers
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,28 +11,35 @@ import android.widget.EditText
 class AvengersActivity : AppCompatActivity() {
 
     var titleName: String? = "Avengers"
-    lateinit var txtMessage: EditText
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var etMessage: EditText
     lateinit var btnSend: Button
+    lateinit var btnLogout: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
         setContentView(R.layout.avengers)
 
-        txtMessage = findViewById(R.id.txtMessage)
-        btnSend = findViewById(R.id.btnSend)
-
-        if(intent != null){
-            titleName = intent.getStringExtra("Name")
-        }
+        titleName = sharedPreferences.getString("Title", "The Avengers") as String
         title = titleName
 
+        etMessage = findViewById(R.id.etMessage)
+        btnSend = findViewById(R.id.btnSend)
+
         btnSend.setOnClickListener{
-            val message = txtMessage.text.toString()
             val intent = Intent(this@AvengersActivity,MessageActivity::class.java)
-            var newMessage = message
-            if(message == null){
-                intent.putExtra("newMessage", newMessage)
-                startActivity(intent)
-            }
+            val message = etMessage.text.toString()
+            intent.putExtra("Message", message)
+            startActivity(intent)
         }
+
+        btnLogout = findViewById(R.id.btnLogout)
+
+        btnLogout.setOnClickListener{
+            startActivity(Intent(this@AvengersActivity,LoginActivity::class.java))
+            sharedPreferences.edit().clear().apply()
+            finish()
+        }
+
     }
 }
